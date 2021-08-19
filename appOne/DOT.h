32c335582661d05722b2ee48d2ec40fe886f70b5
Coding-Ocean <eijo524@gmail.com>
2021-08-19 14:36:51 +0900

    class DOT

---
+++ appOne/DOT.h
@@ -0,0 +1,117 @@
+#pragma once
+#include"libOne.h"
+class DOT {
+    float Hue=0;
+    float Radius=0;
+    VECTOR2 Target;
+    VECTOR2 Pos;
+    VECTOR2 Vel;
+    VECTOR2 Acc;
+    float MaxSpeed=0;
+    float MaxForce=0;
+    float NearDistance=0;
+    float Elapsed = 0;
+public:
+    DOT(float hue, float targetX, float targetY) {
+        //色合いと点の大きさ
+        Hue = hue;
+        Radius = 9;
+        //目標位置
+        Target.x = targetX;
+        Target.y = targetY;
+        //位置は目標位置と同じにしておく
+        Pos = Target;
+    }
+    void setMaxSpeed(float maxSpeed) {
+        MaxSpeed = maxSpeed;
+    }
+    void setMaxForce(float maxForce) {
+        MaxForce = maxForce;
+    }
+    void setNearDistance(float nearDistance) {
+        NearDistance = nearDistance;
+    }
+    void chase(const VECTOR2& target) {
+        //Targetに向かう、望まれる加速度ベクトル
+        VECTOR2 acc = target - Pos;
+        //近づいたらスピードを遅くする
+        float speed = MaxSpeed;
+        float distance = acc.mag();
+        if (distance < NearDistance) {
+            speed = map(distance, 0, NearDistance, 0, MaxSpeed);
+        }
+        //望まれる加速度ベクトルの大きさをspeedにする
+        acc.setMag(speed);
+        //Velを少しtargetに向かわせる加速度ベクトルを求める
+        acc -= Vel;
+        acc.limmit(MaxForce);
+        applyForce(acc);
+    }
+    void runAway(const VECTOR2& target) {
+        //targetから逃げる加速度ベクトル
+        VECTOR2 acc = Pos - target;
+        float distance = acc.mag();
+        if (distance < NearDistance) {
+            acc.setMag(MaxSpeed);
+            acc -= Vel;
+            acc.limmit(MaxForce);
+            applyForce(acc);
+        }
+    }
+    void applyForce(const VECTOR2& force) {
+        Acc += force;
+    }
+    void setRandomPosVel() {
+        //初期位置
+        Pos.x = random(width);
+        Pos.y = random(height);
+        //初速度ベクトル
+        float deg = random(360.0f);
+        angleMode(DEGREES);
+        Vel.x = cos(deg) * 15;
+        Vel.y = sin(deg) * 15;
+    }
+    void update() {
+        setMaxSpeed(20);
+        setMaxForce(1.f);
+        setNearDistance(100);
+        if (isPress(KEY_SPACE)) {
+            Elapsed += delta;
+            if (Elapsed > 0.1f) {
+                setRandomPosVel();
+                Elapsed = 0;
+            }
+        }
+        else if (isPress(KEY_W)) {
+            chase(VECTOR2(width / 2, -Radius));
+        }
+        else if (isPress(KEY_S)) {
+            chase(VECTOR2(width / 2, height + Radius));
+        }
+        else if (isPress(KEY_A)) {
+            chase(VECTOR2(-Radius, height / 2));
+        }
+        else if (isPress(KEY_D)) {
+            chase(VECTOR2(Width + Radius, height / 2));
+        }
+        else {
+            chase(Target);
+            setMaxForce(10);
+            runAway(VECTOR2(mouseX, mouseY));
+            Elapsed = 0.1f;
+        }
+        move();
+    }
+    void move() {
+        Vel += Acc;
+        Pos += Vel;
+        Acc *= 0;
+    }
+    void draw() {
+        colorMode(HSV);
+        angleMode(DEGREES);
+        stroke(Hue, 255, 255);
+        strokeWeight(Radius);
+        point(Pos.x, Pos.y);
+    }
+};
--- appOne/gmain.cpp
+++ appOne/gmain.cpp
@@ -1,125 +1,34 @@
-#include"libOne.h"
-class VEHICLE {
-    VECTOR2 Target;
-    VECTOR2 Pos;
-    VECTOR2 Vel;
-    VECTOR2 Acc;
-    float Radius;
-    float MaxSpeed;
-    float ArriveMaxForce;
-    float FleeMaxForce;
-    float Hue;
-public:
-    VEHICLE(float x, float y, float hue) {
-        //点の大きさと色相
-        Radius = 9;
-        Hue = hue;
-        //目標位置
-        Target.x = x;
-        Target.y = y;
-        //初期位置
-        Pos.x = -Radius;
-        Pos.y = height / 2;
-        //最高速度
-        MaxSpeed = 10;
-        //ハンドルを切る最高の力
-        ArriveMaxForce = 0.5f;
-        FleeMaxForce = 10;
-    }
-    void resetRandomPosVel() {
-        //初期位置
-        Pos.x = random(width);
-        Pos.y = random(height);
-        //初速度ベクトル
-        float deg = random(360.0f);
-        angleMode(DEGREES);
-        Vel.x = cos(deg) * 15;
-        Vel.y = sin(deg) * 15;
-    }
-    void arrive(const VECTOR2& target) {
-        //Targetに向かう、望まれるベクトル
-        VECTOR2 desired = target - Pos;
-        //近づいたらスピードを遅くする
-        float speed = MaxSpeed;
-        float distance = desired.mag();
-        if (distance < 100) {
-            speed = map(distance, 0, 100, 0, MaxSpeed);
-        }
-        //desireの大きさをspeedにする
-        desired.setMag(speed);
-        //ハンドルを切るベクトルを求める
-        VECTOR2 steer = desired - Vel;
-        //Accの大きさをArriveMaxForce以下に制限する
-        steer.limmit(ArriveMaxForce);
-        Acc += steer;
-
-    }
-    void flee(const VECTOR2& target) {
-        //targetから逃げるベクトル
-        VECTOR2 desired = Pos - target;
-        float distance = desired.mag();
-        if (distance < 80) {
-            desired.setMag(MaxSpeed);
-            //加速度ベクトルを求める
-            VECTOR2 steer = desired - Vel;
-            steer.limmit(FleeMaxForce);
-            Acc += steer;
-        }
-    }
-    void update() {
-        if (isTrigger(KEY_SPACE)) {
-            resetRandomPosVel();
-        }
-        else if (isPress(KEY_S)) {
-            arrive(VECTOR2(width/2,height+Radius));
-        }
-        else if (isPress(KEY_A)) {
-            arrive(VECTOR2(-Radius, height / 2));
-        }
-        else {
-            arrive(Target);
-            //マウスカーソルから逃げる
-            flee(VECTOR2(mouseX, mouseY));
-        }
-        Vel += Acc;
-        Pos += Vel;
-        Acc *= 0;
-    }
-    void draw() {
-        colorMode(HSV);
-        angleMode(DEGREES);
-        stroke(Hue,255,255);
-        strokeWeight(Radius);
-        point(Pos.x, Pos.y);
-    }
-};
-
 #include<vector>
-void create(std::vector<VEHICLE>& vehicles) {
+#include"DOT.h"
+
+void create(std::vector<DOT>& dots) {
     //１点、１点、ターゲットの位置（x,y）と色相(hue)を
     //ファイルデータから読み込んで設定する
+    float hue, targetX, targetY;
+    float left = (width - 1450) / 2;
+    float top = (height - 160) / 2;
     FILE* fp;
     fopen_s(&fp, "coding_ocean.csv", "r");
-    float ofsX = (width - 1450) / 2;
-    float ofsY = (height - 160) / 2;
-    float x, y;
-    while (fscanf_s(fp, "%f,%f", &x, &y) != EOF) {
-        float hue = map(y, 0, 160, 150, 210);
-        vehicles.push_back(VEHICLE(x + ofsX, y + ofsY, hue));
+    while (fscanf_s(fp, "%f,%f", &targetX, &targetY) != EOF) {
+        hue = map(targetY, 0, 160, 150, 210);
+        dots.emplace_back(hue, left + targetX, top + targetY);
     }
     fclose(fp);
 }
+
 void gmain() {
     window(1920, 1080, full);
     hideCursor();
-    std::vector<VEHICLE> vehicles;
-    create(vehicles);
+    std::vector<DOT> dots;
+    create(dots);
+    initDeltaTime();
     while (notQuit){
-        clear(1);
+        setDeltaTime();
+        clear();
         point(mouseX, mouseY);
-        for (auto& vehicle : vehicles) {
-            vehicle.update();
-            vehicle.draw();
+        for (DOT& dot : dots) {
+            dot.update();
+            dot.draw();
         }
     }
 }
